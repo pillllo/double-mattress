b49e5ff1bccec8c8251df5f18e453df08c071050
Natalie Pilling <natalie.s.pilling@gmail.com>
2021-11-27 17:16:15 +0100

    feat: create controller for profiles (user, couple) and transactions (user, couple)

--- server/src/controllers/transaction.controller.ts
+++ server/src/controllers/transaction.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
-import * as mockTransactions from "../models/mockTransactions.json";
+import mockTransactions from "../models/mockTransactions";
+import mockUser from "../models/mockUser";
 
-export function getAll(req: Request, res: Response) {
+// Get all transactions from the db
+function getAll(req: Request, res: Response) {
   try {
     const transactions = mockTransactions;
     res.status(200).send(transactions);
@@ -11,29 +13,68 @@ export function getAll(req: Request, res: Response) {
   }
 }
 
-export function addTransaction(req: Request, res: Response) {
+// Get all transactions of the user
+function getAllUser(req: Request, res: Response) {
   try {
-    const transaction = {};
-    res.status(200).send(transaction);
+    const { userId } = req.body;
+    const userTransactions = mockTransactions.filter(
+      (transaction) => transaction.userId === userId
+    );
+    res.status(200).send(userTransactions);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Could not get the list of transactions.");
+  }
+}
+
+// Get all transactions of the couple (user and the partner)
+function getAllCouple(req: Request, res: Response) {
+  try {
+    const { userId } = req.body;
+    // Get id of the partner account linked to the user
+    const partnerId = mockUser.find(
+      (user) => user.userId === userId
+    )?.linkedUserId;
+    // Get user transactions
+    const userTransactions = mockTransactions.filter(
+      (transaction) => transaction.userId === userId
+    );
+    // Get partner transactions
+    const partnerTransactions = mockTransactions.filter(
+      (transaction) => transaction.userId === partnerId
+    );
+    // Combine transactions into couple's transaction
+    const coupleTransactions = userTransactions.concat(partnerTransactions);
+    res.status(200).send(coupleTransactions);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Could not get the list of transactions.");
+  }
+}
+
+// Add a transaction to the db
+function addTransaction(req: Request, res: Response) {
+  try {
+    const transaction = req.body;
+    mockTransactions.push(transaction);
+    res.status(201).send(transaction);
   } catch (error) {
     console.error(error);
     res.status(500).send("Could not add the transaction.");
   }
 }
 
-// export function editTransaction(req: Request, res: Response) {
-//   try {
-//     const transaction = {};
-//     res.status(200).send(transaction);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send("Could not edit the transaction.");
-//   }
-// }
-
-export function deleteTransaction(req: Request, res: Response) {
+// Delete a transaction from the db
+function deleteTransaction(req: Request, res: Response) {
   try {
-    const transaction = {};
+    const { id } = req.params;
+    const transaction = mockTransactions.find(
+      (transaction) => transaction.transactionId === id.toString()
+    );
+    const transactionIndex = mockTransactions.findIndex(
+      (transaction) => transaction.transactionId === id
+    );
+    mockTransactions.splice(transactionIndex, 1);
     res.status(200).send(transaction);
   } catch (error) {
     console.error(error);
@@ -43,8 +84,9 @@ export function deleteTransaction(req: Request, res: Response) {
 
 const transactionController = {
   getAll,
+  getAllUser,
+  getAllCouple,
   addTransaction,
-  // editTransaction,
   deleteTransaction,
 };
 
