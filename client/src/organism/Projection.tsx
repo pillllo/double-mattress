664f90e67b3f0ba18bdcb41114fa2810f40cf183
Liam Morris <LiamMorris96@gmail.com>
2021-11-30 19:58:59 +0000

    Liam front (#38)
    * feat: Added DashboardPieChart & DashboardDatePicker, Mock data has been copied over for ease
    
    * feat: add Accordion component
    
    * feat: first organism, Navbar, added with placeholders and minimal styling
    
    * chore: change icon & update dependencies for Navbar
    
    * feat: connected Dashboard with it's required components
    
    * feat: Added a tastier pie
    
    * chore: fix routing & add path to dashboard
    
    * feat: add accordion to projection page + small text boxes
    
    * feat: Added Projection page
    
    * feat: Added Modal to projection
    
    Co-authored-by: Aothe <[DLiamMorris96@gmail.com>

--- client/src/organism/Projection.tsx
+++ client/src/organism/Projection.tsx
@@ -6,6 +6,7 @@ import {
   Tab,
   TabPanels,
   TabPanel,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import {
@@ -19,58 +20,63 @@ import {
   DateRangeSelector,
   MainButton,
   DashboardDatePicker,
+  ProjectionForm,
 } from "../atoms/index";
 export default function Projection() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Flex
-      h="90%"
-      w="100vw"
-      direction="column"
-      align="center"
-      justify="space-evenly"
-    >
-      <Flex h="35vh" w="90vw">
-        <ProjectionLineChart avgExp={500} avgInc={1200} balance={4000} />
-      </Flex>
-      <Tabs isFitted variant="enclosed" w="100%" h="50%">
-        <TabList>
-          <Tab>Info</Tab>
-          <Tab>Info2</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <Flex
-              direction="column"
-              align="center"
-              justify="space-around"
-              height="40vh"
-            >
-              <MainButton
-                passedFunction={() => alert("hi")}
-                text={"MuhButton"}
+    <>
+      <Flex
+        h="90%"
+        w="100vw"
+        direction="column"
+        align="center"
+        justify="space-evenly"
+      >
+        <Flex h="35vh" w="90vw">
+          <ProjectionLineChart avgExp={500} avgInc={1200} balance={4000} />
+        </Flex>
+        <Tabs isFitted variant="enclosed" w="100%" h="50%">
+          <TabList>
+            <Tab>Monkey</Tab>
+            <Tab>Madness</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Flex
+                direction="column"
+                align="center"
+                justify="space-around"
+                height="35vh"
+              >
+                <MainButton
+                  passedFunction={() => onOpen()}
+                  text={"Add a Projection"}
+                />
+                <DateRangeSelector />
+              </Flex>
+            </TabPanel>
+            <TabPanel>
+              <AccordionItem
+                transaction={{
+                  _id: 1,
+                  transactionId: "1",
+                  transactionType: "income",
+                  userId: "user_a",
+                  otherUserId: "user_b",
+                  amount: 10000,
+                  currency: "EUR",
+                  category: "salary",
+                  date: "2021-12-01T00:00:00.000Z",
+                  description: "Monthly salary",
+                  includeAvg: true,
+                }}
               />
-              <DateRangeSelector />
-            </Flex>
-          </TabPanel>
-          <TabPanel>
-            <AccordionItem
-              transaction={{
-                _id: 1,
-                transactionId: "1",
-                transactionType: "income",
-                userId: "user_a",
-                otherUserId: "user_b",
-                amount: 10000,
-                currency: "EUR",
-                category: "salary",
-                date: "2021-12-01T00:00:00.000Z",
-                description: "Monthly salary",
-                includeAvg: true,
-              }}
-            />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Flex>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+        <ProjectionForm isOpen={isOpen} onClose={onClose} onOpen={onOpen} />
+      </Flex>
+    </>
   );
 }
