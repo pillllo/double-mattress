c4f7ed2e3f39ba187bc69993806bc0bb55ae0eb8
Liam Morris <LiamMorris96@gmail.com>
2021-11-30 19:24:25 +0000

    Liam front (#37)
    * feat: Added DashboardPieChart & DashboardDatePicker, Mock data has been copied over for ease
    
    * feat: add Accordion component
    
    * feat: first organism, Navbar, added with placeholders and minimal styling
    
    * chore: change icon & update dependencies for Navbar
    
    * feat: connected Dashboard with it's required components
    
    * feat: Added a tastier pie
    
    * chore: fix routing & add path to dashboard
    
    * feat: add accordion to projection page + small text boxes
    
    * feat: Added Projection page
    
    Co-authored-by: Aothe <[DLiamMorris96@gmail.com>

--- client/src/organism/Projection.tsx
+++ client/src/organism/Projection.tsx
@@ -1,4 +1,12 @@
-import { Flex } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  TabList,
+  Tabs,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import {
   DashboardTransaction,
@@ -16,31 +24,53 @@ export default function Projection() {
   return (
     <Flex
       h="90%"
+      w="100vw"
       direction="column"
       align="center"
-      mt="15px"
       justify="space-evenly"
     >
-      <Flex h="25">
+      <Flex h="35vh" w="90vw">
         <ProjectionLineChart avgExp={500} avgInc={1200} balance={4000} />
       </Flex>
-      <MainButton passedFunction={() => alert("hi")} text={"MuhButton"} />
-      <DateRangeSelector />
-      <AccordionItem
-        transaction={{
-          _id: 1,
-          transactionId: "1",
-          transactionType: "income",
-          userId: "user_a",
-          otherUserId: "user_b",
-          amount: 10000,
-          currency: "EUR",
-          category: "salary",
-          date: "2021-12-01T00:00:00.000Z",
-          description: "Monthly salary",
-          includeAvg: true,
-        }}
-      />
+      <Tabs isFitted variant="enclosed" w="100%" h="50%">
+        <TabList>
+          <Tab>Info</Tab>
+          <Tab>Info2</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Flex
+              direction="column"
+              align="center"
+              justify="space-around"
+              height="40vh"
+            >
+              <MainButton
+                passedFunction={() => alert("hi")}
+                text={"MuhButton"}
+              />
+              <DateRangeSelector />
+            </Flex>
+          </TabPanel>
+          <TabPanel>
+            <AccordionItem
+              transaction={{
+                _id: 1,
+                transactionId: "1",
+                transactionType: "income",
+                userId: "user_a",
+                otherUserId: "user_b",
+                amount: 10000,
+                currency: "EUR",
+                category: "salary",
+                date: "2021-12-01T00:00:00.000Z",
+                description: "Monthly salary",
+                includeAvg: true,
+              }}
+            />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Flex>
   );
 }
