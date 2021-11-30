69fb921349cbea0ac6adda0106ded33557badd45
Aothe <[DLiamMorris96@gmail.com>
2021-11-30 17:40:22 +0000

    feat: add accordion to projection page + small text boxes

--- client/src/organism/Projection.tsx
+++ client/src/organism/Projection.tsx
@@ -15,15 +15,32 @@ import {
 export default function Projection() {
   return (
     <Flex
-    h="90%"
-    direction="column"
-    align="center"
-    mt="15px"
-    justify="space-evenly"
-  >
-    <ProjectionLineChart avgExp={500} avgInc={1200} balance={4000}/>
-    <MainButton passedFunction={() => alert('hi')} text={"MuhButton"}/>
-    <DateRangeSelector />
-  </Flex>
-  )
+      h="90%"
+      direction="column"
+      align="center"
+      mt="15px"
+      justify="space-evenly"
+    >
+      <Flex h="25">
+        <ProjectionLineChart avgExp={500} avgInc={1200} balance={4000} />
+      </Flex>
+      <MainButton passedFunction={() => alert("hi")} text={"MuhButton"} />
+      <DateRangeSelector />
+      <AccordionItem
+        transaction={{
+          _id: 1,
+          transactionId: "1",
+          transactionType: "income",
+          userId: "user_a",
+          otherUserId: "user_b",
+          amount: 10000,
+          currency: "EUR",
+          category: "salary",
+          date: "2021-12-01T00:00:00.000Z",
+          description: "Monthly salary",
+          includeAvg: true,
+        }}
+      />
+    </Flex>
+  );
 }
