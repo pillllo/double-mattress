2826ae2082bcd6910f13328a3151e75928b04608
David <dlongaron@hotmail.com>
2021-11-30 11:27:05 +0100

    serverless server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,21 @@ import {
   Route,
   Routes
 } from 'react-router-dom';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 const App = function () {
+  //1- Use fetch inside useEffect call API
+  //2- Dispath an action to populate the store with the data
+  //
+  useEffect(()=>{
+    fetch("http://localhost:8888/transactions").then((res)=>{
+      return res.json()
+    }).then((data)=>{
+      useDispatch("GET_DATA",data)
+
+    })
+
+  },[])
   return (
     <Flex
       bgGradient="linear(to-b, gray.600, gray.900)"
--- client/src/actions/displayActions.tsx
+++ client/src/actions/displayActions.tsx
@@ -1,6 +1,12 @@
+//@ts-nocheck
 import { Transaction } from "../types/Transaction";
 
 export type switchDisplay = {
   type: "SWITCH_DISPLAY";
   payload: boolean;
 };
+
+export type getData={
+  type: "GET_DATA";
+  paylod: Transaction[]
+}
