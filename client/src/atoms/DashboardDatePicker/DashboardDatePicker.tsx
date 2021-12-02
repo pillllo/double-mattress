4320e68dabf3bfe73d217425581784bf56624f99
Liam Morris <LiamMorris96@gmail.com>
2021-12-02 20:05:36 +0000

    Liam front (#48)
    * feat: Added DashboardPieChart & DashboardDatePicker, Mock data has been copied over for ease
    
    * feat: add Accordion component
    
    * feat: first organism, Navbar, added with placeholders and minimal styling
    
    * chore: change icon & update dependencies for Navbar
    
    * feat: connected Dashboard with it's required components
    
    * feat: Added a tastier pie
    
    * chore: fix routing & add path to dashboard
    
    * feat: add accordion to projection page + small text boxes
    
    * feat: Added Projection page
    
    * feat: Added Modal to projection
    
    * Add accordion list over projections page
    
    * feat: added dashboard savings bar chart
    
    * feat: Added savings chart to dashboard w/ button
    
    * feat: added Dashboard Bar Chart for savings
    
    * chore: code formatting
    
    * chore: changed styling for dashboard category list
    
    * chore: added Dashboard types
    
    * chore?: add styling
    
    Co-authored-by: Aothe <[DLiamMorris96@gmail.com>

--- client/src/atoms/DashboardDatePicker/DashboardDatePicker.tsx
+++ client/src/atoms/DashboardDatePicker/DashboardDatePicker.tsx
@@ -3,33 +3,34 @@ import { Text } from "@chakra-ui/react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./DashboardDatePicker.css";
-import {useSelector, useDispatch } from "react-redux";
-type Props={
- isDouble:boolean;
-}
+import { useSelector, useDispatch } from "react-redux";
+type Props = {
+  isDouble: boolean;
+};
 
-function DashboardDatePicker({isDouble}:Props) {
-  const dispatch= useDispatch();
+function DashboardDatePicker({ isDouble }: Props) {
+  const dispatch = useDispatch();
   const [startDate, setStartDate] = useState(new Date());
   console.log(startDate);
- const  changeReduxDate=(date:Date)=>{
-    if(isDouble) dispatch({type:"PROJECTION_DATE_CHANGE",payload:date})
-    else dispatch({type:"DASHBOARD_DATE_CHANGE",payload:date})
+  const changeReduxDate = (date: Date) => {
+    if (isDouble) dispatch({ type: "PROJECTION_DATE_CHANGE", payload: date });
+    else dispatch({ type: "DASHBOARD_DATE_CHANGE", payload: date });
 
     setStartDate(date);
-  }
+  };
 
   return (
     <DatePicker
       selected={startDate}
-      onChange={changeReduxDate
-      //   (date: Date) => {
-      //   console.log(Date)
-      //   return setStartDate(date);
-      // }
-    }
-      maxDate={isDouble? null:new Date()}
-      minDate={isDouble? new Date(): null}
+      onChange={
+        changeReduxDate
+        //   (date: Date) => {
+        //   console.log(Date)
+        //   return setStartDate(date);
+        // }
+      }
+      maxDate={isDouble ? null : new Date()}
+      minDate={isDouble ? new Date() : null}
       withPortal
       dateFormat="MM/yyyy"
       showMonthYearPicker
