a81ff056d9f066c86f9e778327a7cbe7c06ddd7c
Aothe <[DLiamMorris96@gmail.com>
2021-12-03 11:26:42 +0000

    feat: Add LandingPage, InfoPage, Testomonials

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import { Flex, Box, Divider, Button } from "@chakra-ui/react";
 import Navbar from "./organism/Navbar";
 import Dashboard from "./organism/Dashboard";
 import Projection from "./organism/Projection";
+import LandingPage from "./organism/LandingPage";
+import Testomonials from "./organism/Testomonials";
+import InfoPage from "./organism/InfoPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,8 +12,6 @@ import { useDispatch, useSelector } from "react-redux";
 const App = function () {
   //1- Use fetch inside useEffect call API
   //2- Dispath an action to populate the store with the data
-
-
 
   return (
     <Flex
@@ -21,8 +22,11 @@ const App = function () {
       <Router>
         <Navbar />
         <Routes>
+          <Route path={`/`} element={<LandingPage />} />
           <Route path={`/dashboard`} element={<Dashboard />} />
           <Route path={`/projections`} element={<Projection />} />
+          <Route path={`/testomonials`} element={<Testomonials />} />
+          <Route path={`/info`} element={<InfoPage />} />
         </Routes>
       </Router>
     </Flex>
---
+++ client/src/organism/InfoPage.tsx
@@ -0,0 +1,49 @@
+import { Box, VStack, Flex, Text, Icon } from "@chakra-ui/react";
+import { FaAddressBook, FaDollarSign, FaFastForward } from "react-icons/fa";
+
+export default function InfoPage() {
+  const data = [
+    {
+      icon: FaAddressBook,
+      title: "Couples Financial Management",
+      content:
+        "Manage your finances with (or without) your partner in one easy-to-use app. Allowing the tracking and viewing of important info & spending based on categories",
+      color: "blue.400",
+    },
+    {
+      icon: FaDollarSign,
+      title: "Savings",
+      content:
+        "Track and view your savings throughout the years, viewing important info such as Amount Saved Per Month, Average Monthly Savings and Projected Savings",
+      color: "green.400",
+    },
+    {
+      icon: FaFastForward,
+      title: "Projections",
+      content:
+        "Take a peek into the future to see your financial standing based on aggregate data. How is that dream holiday to the Maldives going to affect your financial standing? Find out in the Projections section",
+    },
+  ];
+
+  return (
+    <Box p="4" overflowY="auto" h="90%">
+      <Flex direction="column">
+        {data.map((indiv, i) => {
+          return (
+            <VStack key={i} bg="gray.800" my="2rem" p={5} rounded="2xl">
+              <Flex w="14" h="14" aling="center" justify="center">
+                <Icon as={indiv.icon} w={10} h={10} color={indiv.color} />
+              </Flex>
+              <Text fontWeight="700" letterSpacing="wide" fontSize="18">
+                {indiv.title}
+              </Text>
+              <Text align="center" fontWeight="500" fontSize="14">
+                {indiv.content}
+              </Text>
+            </VStack>
+          );
+        })}
+      </Flex>
+    </Box>
+  );
+}
