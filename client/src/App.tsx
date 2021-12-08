5e8f0175d763a84b61bca64ac926d0a53b3838e2
David <dlongaron@hotmail.com>
2021-12-08 16:57:44 +0100

    Payment done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,25 +7,37 @@ import Testimonials from "./organism/Testimonials";
 import InfoPage from "./organism/InfoPage";
 import Subscription from "./organism/Subscription";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import LoginPage from "./organism/LoginPage";
 import { ReduxState } from "./types/ReduxState";
-
+import SubscriptionSuccess from "./organism/SubscriptionSuccess"
 const App = function () {
   //1- Use fetch inside useEffect call API
   //2- Dispath an action to populate the store with the data
   const userObj = useSelector((state: ReduxState) => {
-    if (Array.isArray(state.displayCategories.mainUser)) {
+    console.log(state.displayCategories)
+    //@ts-ignore
+    if (state.displayCategories?.mainUser) {
       //@ts-ignore
-      console.log(state.displayCategories.mainUser[0]);
+      console.log(state.displayCategories.mainUser)
       //@ts-ignore
-      return state.displayCategories.mainUser[0];
+      return state.displayCategories.mainUser
     }
   });
+  const [paid, setPaid]= useState(false);
+  useEffect(() => {
+      //@ts-ignore
+    if(userObj?.activeSubscription){
+      setPaid(true);
+    }
 
-  useEffect(() => {}, [userObj]);
+
+  }, [userObj,paid]);
+  const paidPath= paid ? <Projection /> : <Subscription />
+  console.log(paid)
+  console.log(paidPath);
 
   return (
     <>
@@ -41,16 +53,14 @@ const App = function () {
             <Route path={`/dashboard`} element={<Dashboard />} />
             <Route
               path={`/projections`}
-              element={
-                //@ts-ignore
-                userObj?.activeSubscription ? <Projection /> : <Subscription />
-              }
+              element={paidPath}
             />
             <Route path={`/testimonials`} element={<Testimonials />} />
             <Route path={`/info`} element={<InfoPage />} />
             <Route path={`/login`} element={<LoginPage />} />
             <Route path={`/subscription`} element={<Subscription />} />
             <Route path={`/notifications`} element={<Dashboard />} />
+            <Route path={`/confirm`} element={<SubscriptionSuccess />} />
           </Routes>
         </Router>
       </Flex>
